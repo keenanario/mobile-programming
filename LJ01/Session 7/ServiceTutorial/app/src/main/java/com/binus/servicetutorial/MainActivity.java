5cd8ebb31dfea8e6e94858d45ec8bcba64274468
keenanario <57608848+keenanario@users.noreply.github.com>
2023-12-06 09:49:33 +0700

    Update
    - LG01 -> Session 7,8,10
    - LH01 -> Session 7,8,10

--- LJ01/Session 7/ServiceTutorial/app/src/main/java/com/binus/servicetutorial/MainActivity.java
+++ LJ01/Session 7/ServiceTutorial/app/src/main/java/com/binus/servicetutorial/MainActivity.java
@@ -3,12 +3,14 @@ package com.binus.servicetutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button startBtn, stopBtn, nextPageBtn;
+    SharedPreferences sharedPreferences = getSharedPreferences("ServicePref", MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startBtn.setOnClickListener(this);
         stopBtn.setOnClickListener(this);
         nextPageBtn.setOnClickListener(this);
+
+        boolean flag = sharedPreferences.getBoolean("flagService", false);
+
+        if(!flag){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("flagService", true);
+            editor.apply();
+            startService(service);
+        }
     }
 
     @Override
@@ -30,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.startBtn:
                 startService(service);
+
                 break;
             case R.id.stopBtn:
                 stopService(service);
